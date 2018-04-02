8bcae8e36ba4823af50efb91b694a4e175bdfda5
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-04-01 21:17:26 -0300

    Criando a página de cadastrar

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,31 @@
 module.exports = function(app){
 	app.get('/produtos',function(req,res){		
 		var connection = app.infra.connectionFactory();
-		var produtosBanco = new app.infra.ProdutosDAO(connection);
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-		produtosBanco.lista(function(err,results){
+		produtosDAO.lista(function(err,results){
 			res.render('produtos/lista',{
 				lista:results
 			});
 		});
 
 		connection.end();		
+	});
+
+	app.get('/produtos/form',function(req,res){
+		res.render('produtos/form');
+	});
+
+	app.post('/produtos/salva',function(req,res){
+		var produto = req.body;
+
+		var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		produtosDAO.salva(produto,function(erros,resultados){
+			res.render('produtos/lista');
+		});
+
+		connection.end();
 	});
 }
