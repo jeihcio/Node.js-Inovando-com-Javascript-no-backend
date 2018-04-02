5c4d0705a63122e3fd7c984ee2385cfc24014a73
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-04-01 22:44:13 -0300

    content negotiation

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,22 +1,29 @@
 module.exports = function(app){
-	app.get('/produtos',function(req,res){		
-		var connection = app.infra.connectionFactory();
-		var produtosDAO = new app.infra.ProdutosDAO(connection);
+	app.get('/produtos',function(req,res){
+	    var connection = app.infra.connectionFactory();
+	    var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-		produtosDAO.lista(function(err,results){
-			res.render('produtos/lista',{
-				lista:results
-			});
-		});
+	    produtosDAO.lista(function(erros,resultados){
+	        res.format({
+	            html: function(){
+	                res.render('produtos/lista',{lista:resultados});
+	            },
 
-		connection.end();		
+	            json: function(){
+	                res.json(resultados)
+	            }
+	        });
+
+	    });
+
+	    connection.end();
 	});
 
 	app.get('/produtos/form',function(req,res){
 		res.render('produtos/form');
 	});
 
-	app.post('/produtos/salva',function(req,res){
+	app.post('/produtos',function(req,res){
 		var produto = req.body;
 
 		var connection = app.infra.connectionFactory();
