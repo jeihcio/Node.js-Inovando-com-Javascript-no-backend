a063f131aa7043bab086955dff8f441327539699
Jeihcio Francis <jeihcio.francis@hotmail.com>
2018-04-04 23:26:12 -0300

    refatorando

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,12 +1,23 @@
 var mysql = require('mysql');		
 
 function createDBConnection(){
-	return connection = mysql.createConnection({
-			   host: 'localhost',
-			   user: 'root',
-			   password: 'toor',
-			   database: 'casadocodigo_nodejs'
-		   });
+	if(!process.env.NODE_ENV){
+		return connection = mysql.createConnection({
+				    host: 'localhost',
+			   		user: 'root',
+			   		password: 'toor',
+			   		database: 'casadocodigo_nodejs'
+		   		});
+	}
+
+	if(process.env.NODE_ENV == 'test'){
+		return connection = mysql.createConnection({
+				    host: 'localhost',
+			   		user: 'root',
+			   		password: 'toor',
+			   		database: 'casadocodigo_nodejs_test'
+		   		});
+	}
 }
 
 // wrapper
--- test/produtos.js
+++ test/produtos.js
@@ -1,12 +1,30 @@
 var express = require('../config/express')();
 var request = require('supertest')(express);
 
-describe('ProdutosController',function(){
-	it('listagem json', function(done){
+describe('#ProdutosController',function(){
+	it('#listagem json', function(done){
 		request.get('/produtos')
 			.set('Accept','application/json')
 			.expect('Content-Type',/json/)
 			.expect(200,done);
 	});
 
+	it('#cadastro de novo produto com dados invalidos',function(done){
+		request.post('/produtos')
+			.send({
+				titulo: "",
+				desricao: "novo livro"
+			})
+			.expect(400,done);
+	});
+
+	it('#cadastro de novo produto com dados validos',function(done){
+		request.post('/produtos')
+			.send({
+				titulo: "titulo",
+				desricao: "novo livro",
+				preco: 20.50
+			})
+			.expect(302,done);
+	});
 });
